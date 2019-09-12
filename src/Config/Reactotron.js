2e3fe034424c54c4a8d5bdc13bd1ff5e848d166e
HunWalk <horg3r@gmail.com>
2019-09-12 19:35:51 +0200

    feat: added reactotron

---
+++ src/Config/Reactotron.js
@@ -0,0 +1,10 @@
+import Reactotron from 'reactotron-react-js'
+import { reactotronRedux } from 'reactotron-redux'
+
+
+Reactotron
+  .configure() // we can use plugins here -- more on this later
+  .use(reactotronRedux())
+  .connect() // let's connect!
+
+export default Reactotron
