0d5213235304f3b9b0484e0d828a94753bd3350f
HunWalk <horg3r@gmail.com>
2019-09-12 19:34:47 +0200

    refactor: migrated component code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react'
-import { defaultBreakpoints, generateMedia } from "styled-media-query";
-import ReactSVG from 'react-svg'
 import WebFont from 'webfontloader'
+import { Provider } from 'react-redux'
 
-import { Container, Sidebar, RouterLink, ProfilePic, SidebarSvgContainer } from './Components'
-import { Images, Svgs } from './Theme'
-import Navigator from './Navigation/Navigator'
-import VerticalGroup from './Components/VerticalGroup';
 
-const media = generateMedia(defaultBreakpoints);
+import RootContainer from './RootContainer'
 
+import createStore from './Redux'
+
+const store = createStore()
 
 class App extends Component
 {
@@ -27,33 +25,9 @@ class App extends Component
 
   render(){
     return (
-      <Container>
-        <Sidebar>
-
-          <VerticalGroup>
-            <ProfilePic src={Images.profile} />
-          </VerticalGroup>
-
-          <VerticalGroup>
-            <RouterLink to='/'>Home</RouterLink>
-            <RouterLink to='about'>About</RouterLink>
-          </VerticalGroup>
-
-          <VerticalGroup>
-            <RouterLink to='login'>Login</RouterLink>
-          </VerticalGroup>
-
-        </Sidebar>
-
-        <Navigator />
-
-        <Sidebar>
-          <SidebarSvgContainer>
-            <ReactSVG style={{height: '100vh'}} src={Svgs.motive}/>
-          </SidebarSvgContainer>
-        </Sidebar>
-        
-      </Container>
+      <Provider store={store}>
+        <RootContainer />
+      </Provider>
     );
   }
 
