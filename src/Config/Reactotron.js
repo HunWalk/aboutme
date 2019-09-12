d6ea97b1876da5176cad71994ef33d3f1e4d3d93
HunWalk <horg3r@gmail.com>
2019-09-12 20:19:44 +0200

    feat: adding monitors

--- src/Config/Reactotron.js
+++ src/Config/Reactotron.js
@@ -1,10 +1,16 @@
 import Reactotron from 'reactotron-react-js'
 import { reactotronRedux } from 'reactotron-redux'
-
+import sagaPlugin from 'reactotron-redux-saga'
+import tronsauce from 'reactotron-apisauce'
 
 Reactotron
   .configure() // we can use plugins here -- more on this later
   .use(reactotronRedux())
+  .use(sagaPlugin())
+  .use(tronsauce())
   .connect() // let's connect!
+  .clear()
+
+console.tron = Reactotron
 
 export default Reactotron
