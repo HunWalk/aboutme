09e8bb9a27bb714313d42a127635dc1099ad2bae
Gergely Horvath <horg3r@gmail.com>
2021-01-25 17:55:02 +0100

    feat: dummy content

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
 
-import { Title } from '../Components/'
+import { Title, Description, Container, PageContainer} from '../Components/'
 class About extends Component
 {
     render(){
         return(
-            <Title>Cim</Title>
+            <PageContainer>
+                <Title>Ide kerülhet még bármi</Title>
+                <Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </Description>
+            </PageContainer>
+        
         )
     }
 }
